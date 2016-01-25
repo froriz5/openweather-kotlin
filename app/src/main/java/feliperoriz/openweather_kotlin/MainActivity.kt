40be4432ca24313aa2115791a1b4485f1363afa7
Felipe Roriz <froriz50@gatech.edu>
2016-01-24 19:42:34 -0600

    removed set text from on create.

--- app/src/main/java/feliperoriz/openweather_kotlin/MainActivity.kt
+++ app/src/main/java/feliperoriz/openweather_kotlin/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView.text = "Changed Hello World!"
+        
         button.setOnClickListener(mButtonClicked)
     }
 
