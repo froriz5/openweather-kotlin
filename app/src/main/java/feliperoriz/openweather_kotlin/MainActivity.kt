6b7df4c43ae3d22dc2c7bf3dc6ea091932247b23
Felipe Roriz <froriz50@gatech.edu>
2016-01-24 19:29:26 -0600

    added gradle settings for kotlin.

--- app/src/main/java/feliperoriz/openweather_kotlin/MainActivity.kt
+++ app/src/main/java/feliperoriz/openweather_kotlin/MainActivity.kt
@@ -2,11 +2,13 @@ package feliperoriz.openweather_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = "Changed Hello World!"
     }
 }
